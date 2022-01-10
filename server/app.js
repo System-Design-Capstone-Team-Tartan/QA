a849323e054823c2153d3e3eacf6c9d2a982190a
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-10 17:15:19 -0500

    add morgan to depdencies for logging in dev environment

--- server/app.js
+++ server/app.js
@@ -5,6 +5,11 @@ const utils = require('./lib/utils');
 const app = express();
 
 // Middleware
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(morgan('dev')); // TODO: update in prod
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
