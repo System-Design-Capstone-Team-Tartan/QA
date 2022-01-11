ca0998e474a4b69a135a82f3e3a1e15a1a872204
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-10 20:13:07 -0500

    formatting

--- server/controllers/answers.js
+++ server/controllers/answers.js
@@ -3,11 +3,11 @@
 module.exports = {
   get: (req, res) => {
     const { question_id } = req.params;
-    res.status(200).json({status:'OK'});
+    res.status(200).json({ status: 'OK' });
   },
   post: (req, res) => {
     const { question_id } = req.params;
-    res.status(201).json({status:'Created'});
+    res.status(201).json({ status: 'Created' });
   },
   putHelpful: (req, res) => {
     res.status(204).json({ status: 'No content' });
