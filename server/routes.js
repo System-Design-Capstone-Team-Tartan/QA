6fb8be3e273725b909fb9fa5b849d93647915118
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-11 14:20:28 -0500

    fixed Add an answer route

--- server/routes.js
+++ server/routes.js
@@ -15,32 +15,32 @@ router.get('/questions', controller.questions.get);
 
 // Add a Question:
 // POST /qa/questions
-// status: 201 created
+// status: 201 CREATED
 router.post('/questions', controller.questions.post);
 
-// Answers List
-// GET /qa/questions/:question_id/answers
-// status: 200 OK
-router.get('/questions/:question_id/answers', controller.answers.get);
+// Add an answer
+// POST /qa/questions/:question_id/answers
+// status: 201 CREATED
+router.post('/questions/:question_id/answers', controller.answers.post);
 
 // Mark questions as helpful:
 // PUT /qa/questions/:question_id/helpful
-// Status: 204 no content
+// Status: 204 NO CONTENT
 router.put('/questions/:question_id/helpful', controller.questions.putHelpful);
 
 // Report question:
 // PUT /qa/questions/:question_id/report
-// Status: 204 no content
+// Status: 204 NO CONTENT
 router.put('/questions/:question_id/report', controller.questions.putReport);
 
 // Mark answer as helpful:
 // PUT /qa/answers/:answer_id/helpful
-// Status: 204 no content
+// Status: 204 NO CONTENT
 router.put('/answers/:answer_id/helpful', controller.questions.putHelpful);
 
 // Report answer:
 // PUT /qa/answers/:answer_id/report
-// status: 204 no content
+// status: 204 NO CONTENT
 router.put('/answers/:answer_id/report', controller.questions.putHelpful);
 
 module.exports = router;
