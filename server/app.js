f109a0c9c29bd5fa5d88b97d66f7209652ee13e6
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-10 19:28:17 -0500

    stub out router logic

--- server/app.js
+++ server/app.js
@@ -8,10 +8,17 @@ const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
 
+// Router
+const router = require('./routes');
+
+// Logging and parsing
 app.use(morgan('dev')); // TODO: update in prod
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Set up our routes
+app.use('/qa', router);
 
 app.get('/', (req, res) => {
   res.send('hello world');
