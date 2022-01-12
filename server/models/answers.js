a44bde8b4f077126724c892b78ff9c35acaa762a
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-12 15:26:48 -0500

    implement query method for answers table

--- server/models/answers.js
+++ server/models/answers.js
@@ -1,16 +1,21 @@
 const db = require('../db/postgres');
 
 module.exports = {
-  queryOne: () => {
   // Returns answers for a given question. This list does not include any reported answers.
-  },
-  create: () => {
+  // Should sort by helpfulness w/ seller's answers on top
+  query: (questionId, count = 5, page = 1) => db.query(
+    'SELECT * FROM answers WHERE question_id=$1 AND reported=false ORDER BY helpfulness DESC LIMIT $2 OFFSET $3;',
+    [questionId, count, ((page - 1) * count)],
+  ),
   // Adds an answer for the given question
-  },
-  update: () => {
+  create: (body, name, email, photos) => db.query(
+    'INSERT INTO answers (body, answerer_name, email, photos) VALUES ($1, $2, $3, $4)',
+    [body, name, email, photos],
+  ),
   // Updates an answer to show it was found helpful.
   // Updates an answer to show it has been reported.
   // Note, this action does not delete the answer,
   // but the answer will not be returned in the above GET request.
+  update: () => {
   },
 };
