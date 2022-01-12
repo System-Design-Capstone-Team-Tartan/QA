edc88a762836af928c18539c3065e301156de111
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-12 15:26:07 -0500

    implement get controller for answers table

--- server/controllers/answers.js
+++ server/controllers/answers.js
@@ -1,12 +1,26 @@
-// TODO: var models = require('../models');
+const models = require('../models');
 
 module.exports = {
   get: (req, res) => {
-    const { question_id } = req.params;
-    res.status(200).json({ status: 'OK' });
+    const { question_id: questionId, count, page } = req.params;
+    if (count % 1 !== 0 || count <= 0) {
+      res.status(400).json({ status: 'Error', msg: 'count must be whole number greater than 0' });
+    } else if (page % 1 !== 0 || page <= 0) {
+      res.status(400).json({ status: 'Error', msg: 'page must be whole number greater than 0' });
+    } else {
+      models.answers.query(questionId, count, page)
+        .then((response) => {
+          if (response.length < 1) {
+            res.status(400).json({ status: 'Error', msg: 'No results for question_id' });
+          } else {
+            res.status(200).json({ status: 'OK', data: response });
+          }
+        })
+        .catch((err) => res.status(500).json({ msg: 'Internal database error', err }));
+    }
   },
   post: (req, res) => {
-    const { question_id } = req.params;
+    const { question_id: questionId } = req.params;
     res.status(201).json({ status: 'CREATED' });
   },
   putHelpful: (req, res) => {
