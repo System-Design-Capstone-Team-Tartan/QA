bc05e03d1ab01a1912ca24c6b806e64949eaeb28
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-12 17:45:08 -0500

    update answers.get controller to stringify questionId and format returned data

--- server/controllers/answers.js
+++ server/controllers/answers.js
@@ -2,7 +2,8 @@ const models = require('../models');
 
 module.exports = {
   get: (req, res) => {
-    const { question_id: questionId, count, page } = req.params;
+    const { question_id, count = 5, page = 1 } = req.params;
+    const questionId = question_id.toString();
     if (count % 1 !== 0 || count <= 0) {
       res.status(400).json({ status: 'Error', msg: 'count must be whole number greater than 0' });
     } else if (page % 1 !== 0 || page <= 0) {
@@ -10,10 +11,16 @@ module.exports = {
     } else {
       models.answers.query(questionId, count, page)
         .then((response) => {
-          if (response.length < 1) {
+          const { rows: results } = response;
+          if (!results || results.length < 1) {
             res.status(400).json({ status: 'Error', msg: 'No results for question_id' });
           } else {
-            res.status(200).json({ status: 'OK', data: response });
+            res.status(200).json({
+              status: 'OK',
+              data: {
+                question: questionId, page, count, results,
+              },
+            });
           }
         })
         .catch((err) => res.status(500).json({ msg: 'Internal database error', err }));
@@ -21,6 +28,7 @@ module.exports = {
   },
   post: (req, res) => {
     const { question_id: questionId } = req.params;
+    const { body, name, email, photos } = req.body;
     res.status(201).json({ status: 'CREATED' });
   },
   putHelpful: (req, res) => {
