680c55ca9827db49fed4d769b585f4a447aab668
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-12 21:55:54 -0500

    convert page & count to ints

--- server/controllers/questions.js
+++ server/controllers/questions.js
@@ -2,10 +2,12 @@ const models = require('../models');
 
 module.exports = {
   get: (req, res) => {
-    const {
-      product_id, count = 5, page = 1,
+    let {
+      product_id: productId, count = 5, page = 1,
     } = req.query;
-    const productId = product_id.toString();
+    count = Number(count);
+    page = Number(page);
+    productId = productId.toString();
     if (count % 1 !== 0 || count <= 0) {
       res.status(400).json({ status: 'Error', msg: 'count must be whole number greater than 0' });
     } else if (page % 1 !== 0 || page <= 0) {
@@ -18,7 +20,8 @@ module.exports = {
             res.status(400).json({ status: 'Error', msg: 'No results' });
           } else {
             const promises = questions.map((question) => models.answers.query(
-              question.question_id, 5, 1));
+              question.question_id
+            ));
             Promise.all(promises)
               .then(answers => {
                 // TODO: map the answers into questions as a new property 'answers that's an object
