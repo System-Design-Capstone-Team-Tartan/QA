81f1222503f2ce22f67e0b3cef36298255189022
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-15 14:52:01 -0500

    implement test for POST requests to questions endpoint

--- test/server.spec.js
+++ test/server.spec.js
@@ -3,6 +3,7 @@
 const axios = require('axios');
 const sampleData = require('./sampleData');
 const config = require('../config');
+const db = require('../server/db/postgres');
 
 describe('Q&A GET endpoints', () => {
   it('Should return payload w/ data shape matching sample data (questions endpoint)', (done) => {
@@ -30,25 +31,43 @@ describe('Q&A GET endpoints', () => {
 });
 
 describe('Q&A POST endpoints', () => {
+  afterAll(() => {
+    db.end();
+  }); // TODO: end pool afterwards and determine what async fns are not stopped after tests;
   it('Should add a question to the database (questions endpoint)', (done) => {
-    const payload = {};
+    const payload = {
+      body: 'Can I wash it?',
+      name: 'InquisitiveUser',
+      email: 'IAskQuestions@mail.com',
+      product_id: 36,
+    };
     axios.post(`http://${config.host}:${config.port}/qa/questions`, payload)
-      .then((response) => {
+      .then((data) => {
         // query database for question manually
-        // expect().toEqual();
-        // manually remove entry from database
-        done();
-      });
-  });
-  it('Should add an answer to the database (answers endpoint)', (done) => {
-    const questionId = 1;
-    const payload = {}; //TODO: send
-    axios.get(`http://${config.host}:${config.port}/qa/questions/${questionId}/answers`, payload)
-      .then((response) => {
-        // query database for answer manually
-        // expect().toEqual();
-        // manually remove entry from database
-        done();
+        const queryText = `
+        SELECT asker_name AS name, question_body AS body
+        FROM questions
+        WHERE product_id=$1
+        AND email=$2`;
+        const queryParams = [payload.product_id, payload.email];
+        return db.query(queryText, queryParams)
+          .then((response) => {
+            const { name, body } = response.rows[0];
+            expect(name).toEqual(payload.name);
+            expect(body).toEqual(payload.body);
+          })
+          // delete posted data manually
+          .then((response) => {
+            db.query(
+              'DELETE FROM questions WHERE product_id=$1 AND email=$2',
+              [payload.product_id, payload.email],
+            );
+            done();
+          })
+          .catch((err) => {
+            console.error(err);
+            done();
+          });
       });
   });
 });
