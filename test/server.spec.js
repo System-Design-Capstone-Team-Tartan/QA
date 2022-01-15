ee59cb8986c20ac8d4d237aa316f03bf7caf1f4b
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-15 12:15:12 -0500

    add testing for GET answers endpoint

--- test/server.spec.js
+++ test/server.spec.js
@@ -4,13 +4,27 @@ const axios = require('axios');
 const sampleData = require('./sampleData');
 const config = require('../config');
 
-describe('GET request data shape matches sample data', () => {
-  it('Get questions endpoint response data shape must match sample data', (done) => {
-    axios.get(`http://${config.host}:${config.port}/qa/questions?product_id=36`)
+describe('Q&A GET endpoints', () => {
+  it('Should return payload w/ data shape matching sample data (questions endpoint)', (done) => {
+    const productId = 36;
+    axios.get(`http://${config.host}:${config.port}/qa/questions?product_id=${productId}`)
       .then((response) => {
         const { data } = response.data;
         data.results.forEach((question) => {
           expect(Object.keys(question)).toEqual(Object.keys(sampleData.questions.results[0]));
+        });
+        done();
+      });
+  });
+  it('Should return payload w/ data shape matching sample data (answers endpoint)', (done) => {
+    const questionId = 1;
+    axios.get(`http://${config.host}:${config.port}/qa/questions/${questionId}/answers`)
+      .then((response) => {
+        console.log(response)
+        const { data } = response.data;
+        console.log(data.results)
+        data.results.forEach((answer) => {
+          expect(Object.keys(answer)).toEqual(Object.keys(sampleData.answers.results[0]));
         });
         done();
       });
