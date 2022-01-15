1b0ad94cd367ca995ca237349fc2c61e2dc6b5a8
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-15 11:45:27 -0500

    add test to verify data shape of GET questions endpoint

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,5 +1,21 @@
-describe('Happy trail test', () => {
-  it('Should assert true === true', () => {
-    expect(true).toBe(true);
+/* You'll need to have Postgresql running and your Node server running
+ * for these tests to pass. */
+const axios = require('axios');
+const sampleData = require('./sampleData');
+const config = require('../config');
+
+// const { Pool } = require('pg');
+// const config = require('../config');
+
+describe('GET request data shape matches sample data', () => {
+  it('Get questions endpoint response data shape must match sample data', (done) => {
+    axios.get(`http://${config.host}:${config.port}/qa/questions?product_id=36`)
+      .then((response) => {
+        const { data } = response.data;
+        data.results.forEach((question) => {
+          expect(Object.keys(question)).toEqual(Object.keys(sampleData.questions.results[0]));
+        });
+        done();
+      });
   });
 });
