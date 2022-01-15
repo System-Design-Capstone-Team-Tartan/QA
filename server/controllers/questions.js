af9c9cbf6c4427cc8725b00cdaed084a3eaf1ce1
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-14 22:32:42 -0500

    update questions.get method based on improved query logic

--- server/controllers/questions.js
+++ server/controllers/questions.js
@@ -19,37 +19,14 @@ module.exports = {
           if (!questionsArray || questionsArray.length < 1) {
             res.status(400).json({ status: 'Error', msg: 'No results' });
           } else {
-            const queryAnswers = questionsArray.map((question) => models.answers.query(
-              question.question_id,
-            ));
-            // TODO: Possible optimization: JOINING on question_id might be faster
-            Promise.all(queryAnswers)
-              .then((queryAnswersResponseArray) => {
-                // Map the answersResponseArray into questionsArray as
-                // a new property 'answers that's an object
-                // with key=answer_id and all properties contained
-                const results = questionsArray.map((question, idx) => {
-                  const answersObj = queryAnswersResponseArray[idx].rows.reduce(
-                    (obj, answer) => {
-                      const ans = answer;
-                      ans.id = answer.answer_id;
-                      delete ans.answer_id;
-                      return Object.assign(obj, { [ans.id]: ans });
-                    },
-                    {},
-                  );
-                  const q = question;
-                  q.answers = answersObj;
-                  return q;
-                });
-                res.status(200).json({
-                  status: 'OK',
-                  data: {
-                    product_id: productId,
-                    results,
-                  },
-                });
-              });
+            const results = questionsArray;
+            res.status(200).json({
+              status: 'OK',
+              data: {
+                product_id: productId,
+                results,
+              },
+            });
           }
         })
         .catch((err) => {
