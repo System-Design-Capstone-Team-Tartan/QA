f2a9b3689c740b228d55e6578347c29acefd22cc
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-17 15:14:10 -0500

    partially implement copy script for mongoDB data import from CSV

--- server/db/mongo/copy.js
+++ server/db/mongo/copy.js
@@ -1,36 +1,52 @@
 const path = require('path');
 const fs = require('fs');
-const csv = require('csv-parser');
 const db = require('.');
 
-const f = ['questions.csv', 'answers.csv', 'answers_photos.csv'];
-const filename = f[1];
-const filePath = path.join('..', 'CSV', filename);
-console.log(filePath);
-fs.createReadStream(filePath)
-  .pipe(csv())
-  .on('error', () => {
-    console.log('Yowzers! We have an error!');
-  })
-  .on('data', (data) => {
-    if (filename === 'questions.csv') {
-      db.create(data);
-    } else if (filename === 'answers.csv') {
-      let { question_id } = data;
-      question_id = Number(question_id);
-      db.updateOne(
-        { question_id },
-        { $addToSet: { answers: data } },
-      ).exec();
-    } else if (filename === 'answers_photos.csv') {
-      let { answer_id } = data;
-      answer_id = Number(answer_id);
-      db.updateOne(
-        { answer_id },
-        { $addToSet: { answers: data } },
-      ).exec();
-    }
-  })
-  .on('end', () => {
-    console.log('data loaded');
+// NOTE: it's best to drop the qa database manually
+// before running this script
+
+function promisifiedReadStream(stream, headers) {
+
+  return new Promise((resolve, reject) => {
+    let data = [];
+    stream.on('data', chunk => {
+      let values = chunk.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+      let object = values.reduce((obj, value, idx) => ({ ...obj, [headers[idx]]: value }), {})
+      data.push(object);
+      // console.log(headers);
+      // console.log(object);
+    });
+    stream.on('end', () => resolve(data));
+    stream.on('error', error => reject(error));
   });
+}
+
+function csvToMongoDB(filepath) {
+  const filename = path.parse(filepath).name;
+  const readStream = fs.createReadStream(filepath, 'utf8');
+  const headers = readStream.toString().split('\n')[0]
+  console.log(headers);
+  return promisifiedReadStream(readStream, headers)
+    .then((data) => {
+      if (filename === 'questions') {
+        return db.create(data);
+      }
+      if (filename === 'answers') {
+        let { question_id } = data;
+        question_id = Number(question_id);
+        return db.updateOne({ question_id }, { $addToSet: { answers: data } })
+      }
+      if (filename === 'answers_photos') {
+        let { answer_id } = data;
+        return db.updateOne(
+          {},
+          { $addToSet: { 'answers.$[photos]': data } },
+          { arrayFilters: [{ id: { $eq: answer_id } }] },
+        );
+      }
+    })
+    .catch(err => console.error(err));
+}
+const f = ['questions.csv', 'answers.csv', 'answers_photos.csv'];
+const fp = f.map((filename) => path.join(__dirname, '..', 'CSV', filename));
+csvToMongoDB(fp[0]);
