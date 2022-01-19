6688041768b9202fb6f6289d3afacaf2ca7d4f16
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-19 15:45:35 -0500

    add route to supply test params for loader.io test

--- server/app.js
+++ server/app.js
@@ -25,8 +25,12 @@ app.get('/', (req, res) => {
   res.status(200).json(config.routes || {});
 });
 
+// LOADER.IO TESTING RELATED
 app.get(`/${config.loaderIO}` || '<- loader_io_verification token', (req, res) => {
   res.status(200).send(config.loaderIO);
+});
+app.get('/loader-io/test-params', (req, res) => {
+  res.status(200).json(testParams);
 });
 
 module.exports = app;
