7962183c6cf846d801a08754c302b25cf637f355
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-22 11:34:49 -0500

    remove comments

--- db/postgres/index.js
+++ db/postgres/index.js
@@ -5,8 +5,5 @@ const pool = new Pool(config.postgres);
 pool.connect();
 
 module.exports = {
-  // promise
-  // TODO: call pool.end after shutting down server
   query: (text, params) => pool.query(text, params),
-  end: () => pool.end(),
 };
