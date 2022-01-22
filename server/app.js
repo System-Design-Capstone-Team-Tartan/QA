5ac43a62a3909adaf565a0038cda11d26a2b6f3c
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-22 11:17:58 -0500

    remove unnecessary dependencies

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ const app = express();
 // Middleware
 app.use(compression());
 const morgan = require('morgan');
-const cors = require('cors');
 
 // Router
 const router = require('./routes');
@@ -14,7 +13,6 @@ const config = require('../config');
 
 // Logging and parsing
 app.use(morgan('dev')); // TODO: update in prod
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
